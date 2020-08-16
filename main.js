f12e6cb3b382e47ecfe6704a027a1afe34686c4a
Arne Govaerts <arne.govaerts@student.hogent.be>
2020-08-16 18:04:31 +0200

    site

---
+++ main.js
@@ -0,0 +1,35 @@
+// Offline first
+
+if (navigator.serviceWorker && !navigator.serviceWorker.controller) {
+    navigator.serviceWorker.register('/sw.js');
+}
+
+// Campaigns
+const ref = document.referrer;
+const aTags = document.querySelectorAll("a");
+
+if(ref.includes("instagram")) {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", "instagram");
+    }
+} else if(ref.includes("facebook") || ref.includes("fb")) {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", "facebook");
+    }
+} else if(ref.includes("pinterest")) {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", "pinterest");
+    }
+} else if(ref.includes("twitter") || ref.includes("twimg") || ref.includes("t.co")) {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", "twitter");
+    }
+} else if(ref.includes("linkedin")) {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", "linkedin");
+    }
+} else {
+    for(let a in aTags) {
+        a.href = a.href.replace("unknown_social", ref);
+    }
+}
